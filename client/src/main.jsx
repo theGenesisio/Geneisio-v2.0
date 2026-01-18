6679b223d0f42ecef6d32e473cbf2c9d6724dacb
theGenesisio <genesisioinfo@gmail.com>
2026-01-18 07:29:25 +0100

    feat: Initialize React application entry point with production security measures to disable dev tools and context menus.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,30 +4,30 @@ import "./index.css";
 import App from "./App.jsx";
 const env = import.meta.env.VITE_ENV;
 // Disable text selection, context menu, and devtools
-// if (env === "production") {
-//   document.addEventListener("contextmenu", (e) => e.preventDefault());
-//   document.addEventListener("selectstart", (e) => e.preventDefault());
-//   document.addEventListener("keydown", (e) => {
-//     if (
-//       e.key === "F12" ||
-//       (e.ctrlKey && e.shiftKey && e.key === "I") ||
-//       (e.ctrlKey && e.shiftKey && e.key === "J") ||
-//       (e.ctrlKey && e.key === "U")
-//     ) {
-//       e.preventDefault();
-//     }
-//   });
-//   setInterval(() => {
-//     const before = new Date().getTime();
-//     // eslint-disable-next-line no-debugger
-//     debugger;
-//     const after = new Date().getTime();
-//     if (after - before > 100) {
-//       alert("DevTools is open!");
-//       window.location.href = "/";
-//     }
-//   }, 1000);
-// }
+if (env === "production") {
+  document.addEventListener("contextmenu", (e) => e.preventDefault());
+  document.addEventListener("selectstart", (e) => e.preventDefault());
+  document.addEventListener("keydown", (e) => {
+    if (
+      e.key === "F12" ||
+      (e.ctrlKey && e.shiftKey && e.key === "I") ||
+      (e.ctrlKey && e.shiftKey && e.key === "J") ||
+      (e.ctrlKey && e.key === "U")
+    ) {
+      e.preventDefault();
+    }
+  });
+  setInterval(() => {
+    const before = new Date().getTime();
+    // eslint-disable-next-line no-debugger
+    debugger;
+    const after = new Date().getTime();
+    if (after - before > 100) {
+      alert("DevTools is open!");
+      window.location.href = "/";
+    }
+  }, 1000);
+}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
